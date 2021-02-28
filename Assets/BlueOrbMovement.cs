14a7ac22c349ec08d41248a76d2a419148dd0307
DESKTOP-N0C78OI\Adam <l.d.thompson215@gmail.com>
2021-02-28 14:14:04 +0000

    Level 1 & 2 Complete Orb in level 2 moves between points

---
+++ Assets/BlueOrbMovement.cs
@@ -0,0 +1,73 @@
+﻿using DungeonCrawl.Control;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonCrawl.Items
+{
+    public class BlueOrbMovement : MonoBehaviour
+    {
+        [SerializeField] PatrolPath patrolPath;
+        [SerializeField] float waypointTolerance = 1f;
+        [SerializeField] float waypointDwellTime = 3f;
+
+        float timeSinceArrivedAtWaypoint = Mathf.Infinity;
+        Vector3 orbPosition;
+
+        int currentWaypointIndex = 0;
+
+        private void Start()
+        {
+            orbPosition = transform.position;
+        }
+
+        private void Update()
+        {
+            PatrolBehaviour();
+            UpdateTimers();
+        }
+
+        private void UpdateTimers()
+        {
+            timeSinceArrivedAtWaypoint += Time.deltaTime;
+        }
+    
+
+        private void PatrolBehaviour()
+        {
+            Vector3 nextPosition = orbPosition;
+            if (patrolPath != null)
+            {
+                if (AtWaypoint())
+                {
+                    timeSinceArrivedAtWaypoint = 0;
+                    CycleWaypoint();
+                }
+                nextPosition = GetCurrentWaypoint();
+            }
+            if (timeSinceArrivedAtWaypoint > waypointDwellTime)
+            {
+                transform.position = nextPosition;
+            }
+        }
+
+        private bool AtWaypoint()
+        {
+            //return if at a waypoint
+            float distanceToWaypoint = Vector3.Distance(transform.position, GetCurrentWaypoint());
+            return distanceToWaypoint < waypointTolerance;
+        }
+
+        private void CycleWaypoint()
+        {
+            //select next waypoint to move to
+            currentWaypointIndex = patrolPath.GetNextIndex(currentWaypointIndex);
+        }
+
+        private Vector3 GetCurrentWaypoint()
+        {
+            return patrolPath.GetWaypoint(currentWaypointIndex);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
+
+namespace DungeonCrawl.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.tag == "Player")
+            {
+                int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+                int nextSceneIndex = currentSceneIndex + 1;
+                if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+                {
+                    nextSceneIndex = 0;
+                }
+                SceneManager.LoadScene(nextSceneIndex);
+            }
+        }
+
+    }
+}
