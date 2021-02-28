eb87fd5e5eb09aa47223537cb1c31e0a89fd628c
DESKTOP-N0C78OI\Adam <l.d.thompson215@gmail.com>
2021-02-28 14:43:47 +0000

    Level 4 finished trees are destroyable

---
+++ Assets/DestroyTree.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DungeonCrawl.Control
+{
+    public class DestroyTree : MonoBehaviour
+    {
+        private void OnParticleCollision(GameObject other)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
