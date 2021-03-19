585a56e5adbbcc59027a25c41dc246c8cc95a2b0
DESKTOP-N0C78OI\Adam <l.d.thompson215@gmail.com>
2021-03-19 17:08:08 +0000

    Initial Commit. Basic Prototype created.
    Unity First Person Contoller added - todo, write code for own controller as a walking sim
    Scene handler script created - loads linked scene (currently setup for going up and down stairs, and interacting with an item)
    Standard Assets imported

---
+++ Assets/SceneHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+
+namespace Ancestors.SceneManagement
+{
+    public class SceneHandler : MonoBehaviour
+    {
+        enum destinationidentifier
+        {
+            a, b, c
+        }
+        [SerializeField] destinationidentifier destination;
+
+        [SerializeField] int sceneToLoad = -1;
+        [SerializeField] Transform spawnPoint;
+
+        bool sceneLoadTrigger = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.tag == "Player")
+            {
+                print("Do you want to use the portal?  Y/N ");
+                sceneLoadTrigger = true;
+            }
+        }
+
+
+        private void Update()
+        {
+
+            if (sceneLoadTrigger == true && Input.GetKeyDown(KeyCode.Y))
+            {
+                StartCoroutine(Transition());
+
+            }
+        }
+        IEnumerator Transition()
+        {
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+
+            //gameObject.GetComponent<MeshRenderer>().enabled = false;
+
+            //Transition for scene change
+            
+            SceneHandler otherPortal = GetOtherPortal();
+            UpdatePlayer(otherPortal);
+            yield return new WaitForSeconds(2f);
+            Destroy(gameObject);
+        }
+        private SceneHandler GetOtherPortal()
+        {
+            foreach (SceneHandler portal in FindObjectsOfType<SceneHandler>())
+            {
+                if (portal == this) continue;
+                if (portal.destination != destination) continue;
+                print(portal.name);
+                return portal;
+            }
+            return null;
+        }
+
+        private void UpdatePlayer(SceneHandler otherPortal)
+        {
+            GameObject player = GameObject.FindGameObjectWithTag("Player");
+            print(player.name);
+            print(otherPortal.spawnPoint.name +" " + otherPortal.spawnPoint.position);
+            //unity first person controller defines position so unable to move by other means while components are enabled
+            player.GetComponent<FirstPersonController>().enabled = false;
+            player.GetComponent<CharacterController>().enabled = false;
+
+            player.transform.position = otherPortal.spawnPoint.position;
+
+            player.GetComponent<CharacterController>().enabled = true;
+            player.GetComponent<FirstPersonController>().enabled = true;
+        }
+    }
+}
